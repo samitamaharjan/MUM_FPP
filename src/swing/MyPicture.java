97c57c6997d0cf112be4294e5c13a8863205b828
Samita Maharjan <samitamaharjan@gmail.com>
2017-06-04 10:39:32 -0500

    try and catch added in MyPicture

--- src/swing/MyPicture.java
+++ src/swing/MyPicture.java
@@ -13,61 +13,63 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 @SuppressWarnings("serial")
-public class MyPicture extends JFrame{
-	public void display() throws IOException {
-		// creating label and setting font
-		JLabel label1 = new JLabel();
-		label1.setHorizontalAlignment(JLabel.CENTER);
-		Font font = new Font("TimesRoman", Font.BOLD+Font.ITALIC, 22);
-		label1.setFont(font);
-		
-		// setting image in label
-		Image image = ImageIO.read(getClass().getResource("SNice.png"));
-		Image scaledImage = image.getScaledInstance(200, 200, 0);
-		ImageIcon imageIcon = new ImageIcon(scaledImage);
-		JLabel label2 = new JLabel(imageIcon);
-		
-		label2.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				label1.setText("Bye Samita!");				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				label1.setText("Hello Samita!");
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		this.setLayout(new BorderLayout());
-		this.add(label1, BorderLayout.NORTH);
-		this.add(label2, BorderLayout.CENTER);
-		
-		this.setSize(300, 300);
-		this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class MyPicture extends JFrame {
+	public void display() {
+		try {
+			// creating label and setting font
+			JLabel label1 = new JLabel();
+			label1.setHorizontalAlignment(JLabel.CENTER);
+			Font font = new Font("TimesRoman", Font.BOLD + Font.ITALIC, 22);
+			label1.setFont(font);
+
+			// setting image in label
+			Image image = ImageIO.read(getClass().getResource("SNice.png"));
+			Image scaledImage = image.getScaledInstance(200, 200, 0);
+			ImageIcon imageIcon = new ImageIcon(scaledImage);
+			JLabel label2 = new JLabel(imageIcon);
+
+			label2.addMouseListener(new MouseListener() {
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					label1.setText("Bye Samita!");
+				}
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+					label1.setText("Hello Samita!");
+				}
+
+				@Override
+				public void mouseExited(MouseEvent e) {
+					// TODO Auto-generated method stub
+				}
+
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					// TODO Auto-generated method stub
+				}
+
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// TODO Auto-generated method stub
+				}
+			});
+
+			this.setLayout(new BorderLayout());
+			this.add(label1, BorderLayout.NORTH);
+			this.add(label2, BorderLayout.CENTER);
+
+			this.setSize(300, 300);
+			this.setVisible(true);
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) {
 		MyPicture picture = new MyPicture();
 		picture.display();
 	}
 }
-
